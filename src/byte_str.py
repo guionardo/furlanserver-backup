cad552db37831edf0a027ff00aa3dd9b3246ac6b
Guionardo [GuioNote Debian] <guionardo@gmail.com>
2024-06-09 11:15:36 -0300

    Added byte_str.parse_bytes function

---
+++ src/byte_str.py
@@ -0,0 +1,22 @@
+def _process_comma(b: str) -> str:
+    b = "".join([c for c in b.replace(",", ".") if c == "." or c.isdigit()])
+    parts = b.split(".")
+    if len(parts) <= 2:
+        return b
+    return "".join(parts[:-1]) + "." + parts[-1]
+
+
+def parse_bytes(b: str) -> int:
+    bv = _process_comma(b)
+
+    if b.endswith("K"):
+        mult = 1024
+    elif b.endswith("M"):
+        mult = 1024 * 1024
+    elif b.endswith("G"):
+        mult = 1024 * 1024 * 1024
+    else:
+        mult = 1
+
+    # b = "".join([c for c in b if c == "." or "0" <= c <= "9"])
+    return int(float(bv) * mult)
